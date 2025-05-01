41b2e35c8efe181e0ebebcb38645ed7a8431ef20
Derek Moore <derek.moore@gmail.com>
2025-04-30 22:11:38 -0700

    Extend dynamic animation across both ears instead of mirroring

--- include/Animations/Fuego.h
+++ include/Animations/Fuego.h
@@ -2,10 +2,14 @@
 
 #include "MouseketEars.h"
 
+#define FIRE_WIDTH HW_VIRTUAL_COLS*2
+#define FIRE_HEIGHT HW_VIRTUAL_ROWS
+#define FIRE_CENTER_X (FIRE_WIDTH/2) - 1
+#define FIRE_CENTER_Y (FIRE_HEIGHT/2) - 1
+
 class Fuego : public Animation
 {
 	public:
-		
 		Fuego(const CHSV color = FireOrange_h) :
 			Animation(),
 			mColor(color)
@@ -18,89 +22,88 @@ class Fuego : public Animation
 		{
 		}
 
-		void Setup()
-		{
-		}
-
 		void Loop(LedData *data)
 		{
+			uint32_t now = millis();
+
 			// some changing values
-		  	uint16_t ctrl1 = inoise16(11 * millis(), 0, 0);
-		  	uint16_t ctrl2 = inoise16(13 * millis(), 100000, 100000);
+		  	uint16_t ctrl1 = inoise16(11 * now, 0, 0);
+		  	uint16_t ctrl2 = inoise16(13 * now, 100000, 100000);
 		  	uint16_t  ctrl = ((ctrl1 + ctrl2) / 2);
 
 			// parameters for the heatmap
 			uint16_t speed = 20;
 			x[0] = 3 * ctrl * speed;
-			y[0] = 20 * millis() * speed;
-			z[0] = 5 * millis() * speed ;
+			y[0] = 20 * now * speed;
+			z[0] = 5 * now * speed ;
 			scale_x[0] = ctrl1 / 2;
 			scale_y[0] = ctrl2 / 2;
 
 			// calculate the noise data
 			uint8_t layer = 0;
-			for (uint8_t i = 0; i < HW_VIRTUAL_COLS; i++) {
-				uint32_t ioffset = scale_x[layer] * (i - (HW_VIRTUAL_COLS/2)-1);
-				for (uint8_t j = 0; j < HW_VIRTUAL_ROWS; j++) {
-					uint32_t joffset = scale_y[layer] * (j - (HW_VIRTUAL_ROWS/2)-1);
+			for (uint8_t i = 0; i < FIRE_WIDTH; i++) {
+				uint32_t ioffset = scale_x[layer] * (i - FIRE_CENTER_X);
+				for (uint8_t j = 0; j < FIRE_HEIGHT; j++) {
+					uint32_t joffset = scale_y[layer] * (j - FIRE_CENTER_Y);
 					uint16_t data = ((inoise16(x[layer] + ioffset, y[layer] + joffset, z[layer])) + 1);
 					noise[layer][i][j] = data >> 8;
 				}
 			}
 
 			// parameters for the brightness mask
-			speed = 20;
+			speed = 30;
 			x[1] = 3 * ctrl * speed;
-			y[1] = 20 * millis() * speed;
-			z[1] = 5 * millis() * speed ;
+			y[1] = 20 * now * speed;
+			z[1] = 5 * now * speed ;
 			scale_x[1] = ctrl1 / 2;
 			scale_y[1] = ctrl2 / 2;
 
 			//calculate the noise data
 			layer = 1;
-			for (uint8_t i = 0; i < HW_VIRTUAL_COLS; i++) {
-				uint32_t ioffset = scale_x[layer] * (i - (HW_VIRTUAL_COLS/2)-1);
-				for (uint8_t j = 0; j < HW_VIRTUAL_ROWS; j++) {
-					uint32_t joffset = scale_y[layer] * (j - (HW_VIRTUAL_ROWS/2)-1);
+			for (uint8_t i = 0; i < FIRE_WIDTH; i++) {
+				uint32_t ioffset = scale_x[layer] * (i - FIRE_CENTER_X);
+				for (uint8_t j = 0; j < FIRE_HEIGHT; j++) {
+					uint32_t joffset = scale_y[layer] * (j - FIRE_CENTER_Y);
 					uint16_t data = ((inoise16(x[layer] + ioffset, y[layer] + joffset, z[layer])) + 1);
 					noise[layer][i][j] = data >> 8;
 				}
 			}
 
 			// draw lowest line - seed the fire
-			for (uint8_t x = 0; x < HW_VIRTUAL_COLS; x++) {
-				heat[XY(x, HW_VIRTUAL_ROWS-1)] =  noise[0][HW_VIRTUAL_ROWS-1][7];
+			for (uint8_t x = 0; x < FIRE_WIDTH; x++) {
+				heat[XY(x, FIRE_HEIGHT-1)] = noise[0][FIRE_WIDTH-x][FIRE_CENTER_X];
 			}
 
 			//copy everything one line up
-			for (uint8_t y = 0; y < HW_VIRTUAL_ROWS - 1; y++) {
-				for (uint8_t x = 0; x < HW_VIRTUAL_COLS; x++) {
+			for (uint8_t y = 0; y < FIRE_HEIGHT - 1; y++) {
+				for (uint8_t x = 0; x < FIRE_WIDTH; x++) {
 					heat[XY(x, y)] = heat[XY(x, y + 1)];
 				}
 			}
 
 			//dim
-			for (uint8_t y = 0; y < HW_VIRTUAL_ROWS - 1; y++) {
-				for (uint8_t x = 0; x < HW_VIRTUAL_COLS; x++) {
+			for (uint8_t y = 0; y < FIRE_HEIGHT - 1; y++) {
+				for (uint8_t x = 0; x < FIRE_WIDTH; x++) {
 					uint8_t dim = noise[0][x][y];
 			 		// high value = high flames
 					//dim = dim / 1.7;
-					dim = dim / 3.0;
+					dim = dim / 7.0;
 					dim = 255 - dim;
 					heat[XY(x, y)] = scale8(heat[XY(x, y)] , dim);
 				}
 			}
 
-			for (uint8_t y = 0; y < HW_VIRTUAL_ROWS; y++) {
-				for (uint8_t x = 0; x < HW_VIRTUAL_COLS; x++) {
+			for (uint8_t y = 0; y < FIRE_HEIGHT; y++) {
+				for (uint8_t x = 0; x < FIRE_WIDTH; x++) {
 					// map the colors based on heatmap
 					buf[XY(x,y)] = mColor;
 					buf[XY(x,y)].nscale8(heat[XY(x,y)]);
 
 					// dim the result based on 2nd noise layer
 					buf[XY(x, y)].nscale8(noise[1][x][y]);
-					setPixelXY(data->leftLeds, x, y, buf[XY(x,y)]);
-					setPixelXY(data->rightLeds, x, y, buf[XY(x,y)]);
+					//setPixelXY(data->leftLeds, x, y, buf[XY(x,y)]);
+					//setPixelXY(data->rightLeds, x, y, buf[XY(x,y)]);
+					setWidePixelXY(data, x, y, buf[XY(x,y)]);
 				}
 		 	}
 
@@ -121,10 +124,8 @@ class Fuego : public Animation
 		uint32_t scale_x[numLayers];
 		uint32_t scale_y[numLayers];
 
-		uint8_t noise[numLayers][HW_VIRTUAL_COLS][HW_VIRTUAL_ROWS];
-		uint8_t noise2[numLayers][HW_VIRTUAL_COLS][HW_VIRTUAL_ROWS];
+		uint8_t noise[numLayers][FIRE_WIDTH][FIRE_HEIGHT];
+		uint8_t heat[FIRE_WIDTH*FIRE_HEIGHT];
 
-		CRGB buf[HW_VIRTUAL_COLS*HW_VIRTUAL_ROWS];
-		uint8_t heat[HW_VIRTUAL_COLS*HW_VIRTUAL_ROWS];
-
+		CRGB buf[FIRE_WIDTH*FIRE_HEIGHT];
 };
