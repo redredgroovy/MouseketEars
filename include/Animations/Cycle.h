810ae81f67926911c230d6150b43d1f102609678
Derek Moore <derek.moore@gmail.com>
2025-04-30 22:10:56 -0700

    Add bi-directional animation transition sweep

--- include/Animations/Cycle.h
+++ include/Animations/Cycle.h
@@ -8,19 +8,25 @@
 #include "Animations/Sparkle.h"
 #include "Animations/TwinkleFOX.h"
 
+// Cycle interval in milliseconds
+// This is the time between animation transitions
 #define ANIMATION_INTERVAL_MS 20 * 1000
+
+// Duration of the transition effect in milliseconds
+// This is the time it takes to transition from one animation to the next
 #define ANIMATION_FADE_DURATION_MS 1000
 
+// Array of animations to cycle through in order
 Animation* animationCycle[] = {
 	new Acid(OceanColors_p, 128),
 	new Fuego(PoisonGreen_h),
-	new Sparkle(false, CHSV(0,0,255)),
+	new Sparkle(CHSV(0,0,255)),
 	new Acid(LavaColors_p, 128),
 	new Fuego(IceBlue_h),
 	new TwinkleFOX(FairyLight_p, 128),
 	new TwinkleFOX(Ice_p),
 	new Acid(PoisonGreen_p),
-	new Sparkle(true),
+	new Sparkle(),
 	new Hypno(192),
 	new TwinkleFOX(Snow_p),
 	new Fuego(FireOrange_h),
@@ -29,10 +35,9 @@ Animation* animationCycle[] = {
 class Cycle : public Animation
 {
 	public:
-		
 		Cycle() : Animation()
 		{
-			// Initialize the buffer for transitioning between two animations
+			// Initialize additional buffers for transitioning between two animations
 			mFadeBuffer.rawLeftLeds = (CRGB*)malloc(sizeof(CRGB) * HW_LEDS_PER_EAR);
 			mFadeBuffer.leftLeds = new CRGBSet(mFadeBuffer.rawLeftLeds, HW_LEDS_PER_EAR);
 			mFadeBuffer.rawRightLeds = (CRGB*)malloc(sizeof(CRGB) * HW_LEDS_PER_EAR);
@@ -51,29 +56,38 @@ class Cycle : public Animation
 
 		void Setup()
 		{
+			uint32_t now = millis();
+
 			mStartup = true;
-			mCurrent = 0;
-			mFadeTimer = millis();
-			
+
+			mFadeInterval = now;
+			mFadeStart = now;
 			mInTransition = true;
-			mFadeRatio = 0;
-			mSweepX = 0;
-			mSweepY = 0;
+			mSweepDir = SWEEP_LEFT_TO_RIGHT;
+
+			mCurrent = 0;
 			mNext = 0;
 			animationCycle[mNext]->Setup();
 		}
 
 		void Loop(LedData *data)
 		{
-			unsigned long now = millis();
+			uint32_t now = millis();
 
-			if ( now - mFadeTimer > ANIMATION_INTERVAL_MS ) {
-				mFadeTimer = now;
-
+			// Initiate a new transition if the interval has elapsed
+			if ( now - mFadeInterval > ANIMATION_INTERVAL_MS ) {
+				mFadeInterval = now;
+				mFadeStart = now;
 				mInTransition = true;
-				mFadeRatio = 0;
-				mSweepX = 0;
-				mSweepY = 0;
+
+				// Alternate the sweep direction
+				if ( mSweepDir == SWEEP_LEFT_TO_RIGHT ) {
+					mSweepDir = SWEEP_RIGHT_TO_LEFT;
+				} else {
+					mSweepDir = SWEEP_LEFT_TO_RIGHT;
+				}
+
+				// Prime the incoming animation
 				mNext = (mCurrent + 1) % ARRAY_SIZE(animationCycle);
 				animationCycle[mNext]->Setup();
 			}
@@ -90,38 +104,65 @@ class Cycle : public Animation
 				// Render a frame of the incoming animation
 				animationCycle[mNext]->Loop(&mFadeBuffer);
 
-				// Sweep from left to right within ANIMATION_FADE_DURATION
-				EVERY_N_MILLISECONDS(ANIMATION_FADE_DURATION_MS / (HW_VIRTUAL_COLS*2)) {
-					mSweepX++;
-					if ( mSweepX >= (HW_VIRTUAL_COLS*2) ) {
-						mStartup = false;
-						mInTransition = false;
-						mCurrent = mNext;
-					}
+				// Map the fade progress to a value between 0 and HW_VIRTUAL_COLS*2 based
+				// on elapsed time and the duration of the transition
+				float elapsed = now - mFadeStart;
+				float progress = min( (elapsed/ANIMATION_FADE_DURATION_MS) * 255.0, 255.0 );
+				if ( progress >= 255.0 ){
+					mStartup = false;
+					mInTransition = false;
+					mCurrent = mNext;
 				}
+				int16_t sweepX = scale8(HW_VIRTUAL_COLS*2, (uint8_t)progress); // 0-32
 
-				// Sweep the left ear...
-				if ( mSweepX < HW_VIRTUAL_COLS ) {
-					for ( uint8_t i = 0; i < data->leftLeds->size(); i++ ) {
-						if ( HW_V_COORDS_X[i] < mSweepX ) {
-							(*data->leftLeds)[i] = CRGB((*mFadeBuffer.leftLeds)[i]);
+				if ( mSweepDir == SweepDirection::SWEEP_LEFT_TO_RIGHT ) {
+					// Sweep the left ear...
+					if ( sweepX < HW_VIRTUAL_COLS ) {
+						for ( uint8_t i = 0; i < data->leftLeds->size(); i++ ) {
+							if ( HW_V_COORDS_X[i] < sweepX ) {
+								(*data->leftLeds)[i] = CRGB((*mFadeBuffer.leftLeds)[i]);
+							}
+						}
+					// ...then the right ear
+					} else {
+						(*data->leftLeds) = (*mFadeBuffer.leftLeds);
+						for ( uint8_t i = 0; i < data->rightLeds->size(); i++ ) {
+							if ( HW_V_COORDS_X[i] < sweepX - HW_VIRTUAL_COLS ) {
+								(*data->rightLeds)[i] = CRGB((*mFadeBuffer.rightLeds)[i]);
+							}
 						}
 					}
-				// ...then the right ear
-				} else {
-					(*data->leftLeds) = (*mFadeBuffer.leftLeds);
-					for ( uint8_t i = 0; i < data->rightLeds->size(); i++ ) {
-						if ( HW_V_COORDS_X[i] < mSweepX - HW_VIRTUAL_COLS ) {
-							(*data->rightLeds)[i] = CRGB((*mFadeBuffer.rightLeds)[i]);
+					// Overlay the transition line and sparkles
+					for ( int y = 0; y < HW_VIRTUAL_ROWS; y++ ) {
+						setWidePixelXY(data, sweepX, y, CHSV(Gold_h.h,255,192));
+						if ( random(5) == 1 ) {
+							setWidePixelXY(data, sweepX-1, y, CHSV(0,0,192));
 						}
 					}
-				}
-
-				// Overlay the transition line and sparkles
-				for ( int y = 0; y < HW_VIRTUAL_ROWS; y++ ) {
-					setWidePixelXY(data, mSweepX, y, CHSV(Gold_h.h,255,192));
-					if ( mSweepX > 0 && random(5) == 1 ) {
-						setWidePixelXY(data, mSweepX-1, y, CHSV(0,0,192));
+				} else { // mSweepDir == SweepDirection::SWEEP_RIGHT_TO_LEFT
+					sweepX = (HW_VIRTUAL_COLS*2) - 1 - sweepX; // Invert the sweep direction
+					// Sweep the right ear...
+					if ( sweepX >= HW_VIRTUAL_COLS ) {
+						for ( uint8_t i = 0; i < data->rightLeds->size(); i++ ) {
+							if ( HW_V_COORDS_X[i] > sweepX - HW_VIRTUAL_COLS ) {
+								(*data->rightLeds)[i] = CRGB((*mFadeBuffer.rightLeds)[i]);
+							}
+						}
+					// ...then the left ear
+					} else {
+						(*data->rightLeds) = (*mFadeBuffer.rightLeds);
+						for ( uint8_t i = 0; i < data->leftLeds->size(); i++ ) {
+							if ( HW_V_COORDS_X[i] > sweepX ) {
+								(*data->leftLeds)[i] = CRGB((*mFadeBuffer.leftLeds)[i]);
+							}
+						}
+					}
+					// Overlay the transition line and sparkles
+					for ( int y = 0; y < HW_VIRTUAL_ROWS; y++ ) {
+						setWidePixelXY(data, sweepX, y, CHSV(Gold_h.h,255,192));
+						if ( random(5) == 1 ) {
+							setWidePixelXY(data, sweepX+1, y, CHSV(0,0,192));
+						}
 					}
 				}
 			}
@@ -134,11 +175,19 @@ class Cycle : public Animation
 		bool mStartup = true;
 		bool mInTransition = false;
 
-		int16_t mFadeRatio = 0;
-		uint16_t mSweepX = 0;
-		uint16_t mSweepY = 0;
-	  
+		enum SweepDirection {
+			SWEEP_LEFT_TO_RIGHT = 0,
+			SWEEP_RIGHT_TO_LEFT = 1,
+			SWEEP_UP = 2,
+			SWEEP_DOWN = 3,
+		};
+
+		int16_t mSweepX = 0;
+		SweepDirection mSweepDir = SWEEP_RIGHT_TO_LEFT;
+
 		LedData mFadeBuffer;
-		unsigned long mFadeTimer = 0;
+		uint32_t mFadeInterval = 0;
+		uint32_t mFadeStart = 0;
+
 
 };
